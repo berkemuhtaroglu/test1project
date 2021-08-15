777466d279c8f372239594a1dc4efd7b6d262f54
Berke Muhtaroglu <berkemuhtaroglu@gmail.com>
2021-08-15 20:52:44 +0300

    Add files via upload

---
+++ HRApplication.SOAPServices/Models/DBModels/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRApplication.SOAPServices.Models.DBModels
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string City { get; set; }
+        public double Salary { get; set; }
+        public string Manager { get; set; }
+
+    }
+}
